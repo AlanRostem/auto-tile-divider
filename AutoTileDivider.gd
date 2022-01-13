73981e92e2ccb8ba6b9cb9d963f054ad3182efd6
Alan Rostem <arostem@gmail.com>
2022-01-14 00:30:45 +0100

    endelig

--- AutoTileDivider.gd
+++ AutoTileDivider.gd
@@ -1,13 +1,35 @@
 tool
 extends Control
 
-const COLS = 5
-#const ROWS = 3
-
 export(Texture) var __tile_set: Texture setget __set_tile_set
 
-func __set_tile_set(tex):
-	__tile_set = tex
-	var tile_set = $TileMap.tile_set
+onready var __view_port = $ViewportContainer/Viewport
+onready var __save_dialog = $SaveDialog
+onready var __open_dialog = $OpenDialog
+
+func __set_tile_set(tile_set):
+	__tile_set = tile_set
+	#__update_tiles(tile_set)
+	
+func _ready():
+	__open_dialog.popup_centered()
+
+func __save(path):
+	var img = __view_port.get_texture().get_data()
+	img.flip_y()
+	img.save_png(path)
+	
+func __load(path):
+	__update_tiles(load(path))
+	
+func __update_tiles(image):
+	var tile_set = $ViewportContainer/Viewport/TileMap.tile_set
 	for id in tile_set.get_tiles_ids():
-		tile_set.tile_set_texture(id, __tile_set)
+		tile_set.tile_set_texture(id, image)
+
+func _on_OpenDialog_file_selected(path):
+	__load(path)
+	__save_dialog.call_deferred("popup_centered")
+
+func _on_SaveDialog_file_selected(path):
+	__save(path)
